d3fb9ae52e803a841072229f63bb470e75ec5ed6
Noah Harris <noahjharris@outlook.com>
2024-02-26 11:27:49 +1100

    started

---
+++ Assets/HallwayPiece.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallwayPiece : MonoBehaviour
+{
+    enum HallwayType
+    {
+        straight,
+        straightCloset,
+        straightPopout,
+        turnLeft,
+        turnRight
+    }
+}
